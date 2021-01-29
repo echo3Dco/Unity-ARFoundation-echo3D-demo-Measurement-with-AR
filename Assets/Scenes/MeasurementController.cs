3ff9c09eadcd8a46fdffd426d5e6733fc2ec0c8b
bpedazur <bpedazur@gmail.com>
2021-01-29 11:39:58 -0500

    first commit

---
+++ Assets/Scenes/MeasurementController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class MeasurementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject measurementPointPrefab;
+
+    [SerializeField]
+    private float measurementFactor = 39.37f;
+
+    [SerializeField]
+    private Vector3 offsetMeasurement = Vector3.zero;
+
+    [SerializeField]
+    private TextMeshPro distanceText;
+
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+    
+    private ARSessionOrigin arOrigin;
+
+    private LineRenderer measureLine;
+
+    private ARRaycastManager arRaycastManager;
+
+    private GameObject startPoint;
+
+    private GameObject endPoint;
+
+    private Vector2 touchPosition = default;
+    
+    private bool activated = true;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+
+        startPoint = Instantiate(measurementPointPrefab, Vector3.zero, Quaternion.identity);
+        endPoint = Instantiate(measurementPointPrefab, Vector3.zero, Quaternion.identity);
+        Debug.Log("Started");
+   
+        measureLine = GetComponent<LineRenderer>();
+        Debug.Log("Passed");
+        startPoint.SetActive(false);
+        endPoint.SetActive(false);
+
+        Debug.Log("Finished awake");
+    }
+    
+    void Update()
+    { 
+        if(activated && Input.touchCount > 0)
+        {
+            Debug.Log("Touch detected");
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch began");
+                touchPosition = touch.position;
+
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    Debug.Log("within plane");
+                    startPoint.SetActive(true);
+
+                    Pose hitPose = hits[0].pose;
+                    startPoint.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                }
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("Touch moved");
+                touchPosition = touch.position;
+                
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    Debug.Log("Within plane2");
+                    measureLine.gameObject.SetActive(true);
+                    endPoint.SetActive(true);
+
+                    Pose hitPose = hits[0].pose;
+                    endPoint.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+
+        if(startPoint.activeSelf && endPoint.activeSelf)
+        {
+            Debug.Log("Try calc distance");
+            distanceText.transform.position = endPoint.transform.position + offsetMeasurement;
+            distanceText.transform.rotation = endPoint.transform.rotation;
+            measureLine.SetPosition(0, startPoint.transform.position);
+            measureLine.SetPosition(1, endPoint.transform.position);
+
+            distanceText.text = $"Distance: {(Vector3.Distance(startPoint.transform.position, endPoint.transform.position) * measurementFactor).ToString("F2")} in";
+        }
+    }
+
+    public void ActivateMode() {
+        activated = true;
+    }
+
+    public void DeactivateMode() {
+        activated = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# MeasureAR
+Simple measurement application using AR Foundation, Unity, and EchoAR. Tested on Android devices.
+
+You can measure custom AR objects:
+
+![ar_measure](/Screenshots/AR_object_measure.jpg)
+
+Or real-life objects:
+
+![measure](/Screenshots/real_object_measure.jpg)
+
+
+## Register
+If you don't have an echoAR API key yet, make sure to register for FREE at [echoAR](https://console.echoar.xyz/#/auth/register).
+
+## Setup
+* Clone this repository for prefabs,scenes and custom scripts.
+* Open the project in Unity and follow the instructions on our [doumention page](https://docs.echoar.xyz/unity/adding-ar-capabilities) to [set your API key](https://docs.echoar.xyz/unity/adding-ar-capabilities#3-set-you-api-key).
+* Set your echoAR API key in the echoAR prefab
+* Add models to your echoAR project
+
+## Run
+* [Build and run the AR application](https://docs.echoar.xyz/unity/adding-ar-capabilities#4-build-and-run-the-ar-application).
+
+## Usage Instructions:
+The app has two modes:
+* Object Placement mode, which allows you to place an EchoAR object on any plane
+* Measurement Tape mode, which allows you to drag your finger between any two points on a plane to create a measurement.
+
+- You can easily switch between modes by clicking on their respective buttons in the UI. 
+- Note that because both modes depend on plane detection, it may take a few seconds to move your camera around before the plane you are trying to measure may register.
+- Remember: if the placed Object is too large, you can modify the scale of the object in the EchoAR console (see https://docs.echoar.xyz/unity/transforming-content)
